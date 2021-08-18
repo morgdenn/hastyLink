2af45f2bee597004cb617f7f543273cae6fc1522
Morgan Dennithorne <morgan@pal.care>
2021-08-18 17:36:19 +1000

    removed TS

--- bookmark.js
+++ bookmark.js
@@ -1,16 +1,28 @@
-javascript: (function () {
-    var defaultEmail = '';
-    var body = '';
-    if (window.getSelection) {
-        body = window.getSelection().toString();
-    }
-    if (encodeURI(body).length === 0 && document.getElementsByName('description')[0]) {
-        body = document.getElementsByName('description')[0].getAttribute('content');
-    }
-    var su = document.title;
-    su = su.replace('«', ' ');
-    su = su.replace(/(\xbb|\xa0|\0|\n|\f|\r|\t|\v)/gmi, ' ');
-    su = encodeURI(su);
-    window.open('https://mail.google.com/mail?view=cm&tf=0&to=' + defaultEmail + '&su=' + su + '&body=' + encodeURI(body) + '%0A%0A' + encodeURI(location.href), 'gmailForm', 'scrollbars=yes,width=680,height=510,top=175,left=75,status=no,resizable=yes');
-    return undefined;
+(function () {
+  var body = '';
+
+  // Has any text been selcted
+  if (window.getSelection) {
+    body = window.getSelection().toString();
+  }
+
+  // Get the page description.
+  if (body.length === 0 && document.getElementsByName('description')[0]) {
+    body = document.getElementsByName('description')[0].getAttribute('content');
+  }
+
+  // Get the page title
+  var subject = document.title
+    .replace('«', ' ')
+    .replace(/(\xbb|\xa0|\0|\n|\f|\r|\t|\v)/gim, ' ');
+
+  window.open(
+    'mailto:?subject=' +
+      encodeURIComponent(subject) +
+      '&body=' +
+      encodeURIComponent(body) +
+      escape('\r\n') +
+      encodeURIComponent(location.href),
+    '_blank'
+  );
 })();
