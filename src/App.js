a2b6736817fa8d342def2309debde223ca958927
Morgan Dennithorne <morgan@pal.care>
2021-08-24 09:49:23 +1000

    link working and title

--- src/App.js
+++ src/App.js
@@ -5,13 +5,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="javascript:!function(){var e='';window.getSelection&&(e=window.getSelection().toString()),0===e.length&&document.getElementsByName('description')[0]&&(e=document.getElementsByName('description')[0].getAttribute('content'));var n=document.title.replace('«',' ').replace(/(\xbb|\xa0|\0|\n|\f|\r|\t|\v)/gim,' ');window.open('mailto:?subject='+encodeURIComponent(n)+'&body='+encodeURIComponent(e)+escape('\r\n')+encodeURIComponent(location.href),'_blank')}();"
           target="_blank"
           rel="noopener noreferrer"
         >
